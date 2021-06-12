7a838fe7780624f8fcb4103d70bc0946e47911dd
Raul Velasco <raul93611@gmail.com>
2021-06-11 20:26:30 -0400

    small things to do when the project is recovered

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/raul-hp/Documentos/web/adminblog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/raul-hp/Documentos/web/adminblog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/raul-desktop/Documentos/projects/blog/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/raul-desktop/Documentos/projects/blog/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
